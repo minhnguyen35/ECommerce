0cc57a3346c59671a3169990dd22ce436d7bc55a
LHN2225 <50903203+LHN2225@users.noreply.github.com>
2020-09-18 23:10:42 +0700

    UserInfo + UserOrder + OrderItem

---
+++ app/src/main/java/com/example/ecommerce/Order_Item.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce;
+
+public class Order_Item {
+    private String id;
+    private String itemName;
+    private int quantity;
+    private float total;
+
+    /****************************************************************************************/
+
+}
--- app/src/main/java/com/example/ecommerce/UserAccount.java
+++ app/src/main/java/com/example/ecommerce/UserAccount.java
@@ -1,14 +1,53 @@
 package com.example.ecommerce;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class UserAccount {
-    private String username;
-    private String password;
-    private String phoneNumber;
+    private User_Info userInfo;
+    private ArrayList<User_Order> userOrders;
     private String dateRegister;
 
-    public String getUsername() {
+    /****************************************************************************************/
+
+    public UserAccount(User_Info userInfo, ArrayList<User_Order> userOrders, String dateRegister) {
+        this.userInfo = userInfo;
+        this.userOrders = userOrders;
+        this.dateRegister = dateRegister;
+    }
+
+    public void addOrder(User_Order newOrder){
+        userOrders.add(newOrder);
+    }
+
+    public User_Info getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(User_Info userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public ArrayList<User_Order> getUserOrders() {
+        return userOrders;
+    }
+
+    public void setUserOrders(ArrayList<User_Order> userOrders) {
+        this.userOrders = userOrders;
+    }
+
+    public String getDateRegister() {
+        return dateRegister;
+    }
+
+    public void setDateRegister(String dateRegister) {
+        this.dateRegister = dateRegister;
+    }
+
+/*private String username;
+    private String password;
+    private String phoneNumber;*/
+    /*public String getUsername() {
         return username;
     }
 
@@ -26,7 +65,5 @@ public class UserAccount {
         this.password = password;
         this.phoneNumber = phoneNumber;
         this.dateRegister = dateRegister;
-    }
-
-
+    }*/
 }
